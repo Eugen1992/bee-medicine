bded90a80b160e421401d817b6498d790497c774
Eugen ALforov <eugenalforov@gmail.com>
2016-04-07 17:10:39 +0300

    fixed sending emails

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -10,32 +10,31 @@ function controller(app) {
    }
  });
   
-  var recievers = [
-                {recieverAdress: 'eugenalforov@gmail.com', id: 1}
-                ]
   app.post("/contact/booktime", function(req, res){ 
     var recieversAdreses = [];
 
-    _.each(recievers, function(reciever) {
-       recieversAdreses.push(reciever.recieverAdress);
+    req.db.collection('receivers').
+      find({}).
+      toArray(function (err, records) {
+        var joinedAdreses = records.map(function (receiver) { return receiver.recieverAdress }).join(', ');
+    
+        var mailOptions = {
+         from: 'Сон на ульях  <beesmedicine@gmail.com>', // sender address
+         to: joinedAdreses, // list of receivers
+         subject: 'Заказ времени и услуги', // Subject line
+         text: 'Заказ  ' + '\n\n' + 'Имя клиента: ' + req.body.customerName + '\n\n' +  'Контактный номер телефона: ' + req.body.customerPhone + '\n\n' + 'Дата: ' + req.body.year + '.' + req.body.month + '.'  + req.body.day + '\n\n' + 'Время: ' + req.body.time // plaintext body
+        }
+
+        transporter.sendMail(mailOptions, function(error, info){ 
+         if (error) {
+           res.sendStatus(500);
+         } else {
+           res.send(JSON.stringify(req.body));
+         }
+      });
     });
 
-    var joinedAdreses = recieversAdreses.join(', ');
     
-    var mailOptions = {
-     from: 'noreply  <dimahrytsenko@gmail.com>', // sender address
-     to: joinedAdreses, // list of receivers
-     subject: 'Заказ времени и услуги', // Subject line
-     text: 'Заказ  ' + '\n\n' + 'Имя клиента: ' + req.body.customerName + '\n\n' +  'Контактный номер телефона: ' + req.body.customerPhone + '\n\n' + 'Дата: ' + req.body.year + '.' + req.body.month + '.'  + req.body.day + '\n\n' + 'Время: ' + req.body.time // plaintext body
-    }
-    
-    transporter.sendMail(mailOptions, function(error, info){ 
-     if (error) {
-       res.sendStatus(500);
-     } else {
-       res.send(JSON.stringify(req.body));
-     }
-    });
   });
 }
 
